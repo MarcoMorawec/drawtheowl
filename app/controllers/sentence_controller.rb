d72ef83beca28aa0fb51f47daff20babc0c16303
marcomorawec <marco.morawec@gmail.com>
2013-06-23 17:08:49 -0400

    adding random selection action to index

--- app/controllers/sentence_controller.rb
+++ app/controllers/sentence_controller.rb
@@ -1,6 +1,15 @@
 class SentenceController < ApplicationController
   
   def index
-  	@sentence = Sentence.all
+  	@sentence = Sentence.first(:offset => rand(Sentence.count))
   end
+
+  def next
+  	@sentence = Sentence.first(:offset => rand(Sentence.count))
+  		render ('index')
+  end
+
+
+
+
 end
