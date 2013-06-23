7727e12789b40d6c1db65a1c7a04413b2acb040a
marcomorawec <marco.morawec@gmail.com>
2013-06-23 18:20:52 -0400

    adding edit and list views and adding the list, new , edit and create actions to the controller

--- app/controllers/sentence_controller.rb
+++ app/controllers/sentence_controller.rb
@@ -9,6 +9,10 @@ class SentenceController < ApplicationController
   		render ('index')
   end
 
+  def list123
+    @sentence = Sentence.all
+  end
+
   def new123
     @sentence = Sentence.new(params[:sentence])
   end
@@ -16,15 +20,26 @@ class SentenceController < ApplicationController
   def create
   	@sentence = Sentence.new(params[:sentence])
     if @sentence.save
-      flash[:notice] = "We saved your sentence!"
+      flash[:notice] = "We saved your advice!"
       redirect_to(:action => 'index')
     else
       render('new123')
     end
   end
 
+  def edit123
+    @sentence = Sentence.find(params[:id])
+  end
 
-
+  def update
+    @sentence = Sentence.find(params[:id])
+    if @sentence.update_attributes(params[:sentence])
+      flash[:notice] = "Your advice is updated!"
+      redirect_to(:action => 'list123')
+    else
+      render('edit')
+    end
+  end
 
 
 
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,4 +1,8 @@
 class Sentence < ActiveRecord::Base
   attr_accessible :name, :twitterhandle, :email, :advice
+
+  validates :twitterhandle, presence: true
+  validates :advice, presence: true
+  
 end
 
