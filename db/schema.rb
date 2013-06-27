85f9362258edc0a30cb1960074f07e582ec7f1b6
marcomorawec <marco.morawec@gmail.com>
2013-06-27 18:45:55 -0400

    development migration rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623191258) do
-
-  create_table "sentences", :force => true do |t|
-    t.string   "name"
-    t.string   "twitterhandle"
-    t.string   "email",         :default => "",    :null => false
-    t.text     "advice"
-    t.boolean  "visible",       :default => false
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
